4ad43b7b3e8629119203a20cb55c4051d779ba78
leiming <leiming_student@163.com>
2023-04-21 00:10:33 +0800

    配置全局logger

--- log.py
+++ log.py
@@ -2,10 +2,17 @@ import logging
 
 
 def setup_custom_logger(name):
+    """
+    公用日志配置
+    :param name: 日志某块名
+    :return: logger
+    """
+    # 设置日志格式
     formatter = logging.Formatter(fmt='%(asctime)s - %(levelname)s - %(module)s - %(message)s')
     handler = logging.StreamHandler()
     handler.setFormatter(formatter)
     logger = logging.getLogger(name)
-    logger.setLevel(logging.DEBUG)
     logger.addHandler(handler)
+    # 设置日志等级
+    logger.setLevel(logging.DEBUG)
     return logger
