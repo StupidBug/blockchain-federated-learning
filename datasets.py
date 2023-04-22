b8dc9205142ba09fe74d7d37d7bd0fbd2f274314
leiming <leiming_student@163.com>
2023-04-22 15:57:42 +0800

    添加node数据集类

--- datasets.py
+++ datasets.py
@@ -1,11 +1,21 @@
+import torch
 import torch.utils.data as data
 import numpy as np
+from torch.utils.data.dataset import T_co
 from torchvision.datasets import CIFAR10
+from torch.utils.data import TensorDataset
+import log
+
+logger = log.setup_custom_logger("dataset")
+dataset_suffix = ".dataset"
 
 
-class Cifar10Truncated(data.Dataset):
+class GlobalDataset(data.Dataset):
+    """
+    cifar10数据集，继承 data.Dataset 类，可被用于生成 dataloader 进行训练
+    """
 
-    def __init__(self, root, dataidxs=None, train=True, transform=None, target_transform=None, download=False):
+    def __init__(self, root, dataidxs=None, train=True, transform=None, target_transform=None, name=None):
         """
         构造 cifar10 数据集，如果没有就下载
         """
@@ -15,13 +25,13 @@ class Cifar10Truncated(data.Dataset):
         self.train = train
         self.transform = transform
         self.target_transform = target_transform
-        self.download = download
-
+        self.name = name
         self.data, self.target = self.__build_truncated_dataset__()
 
     def __build_truncated_dataset__(self):
 
-        cifar_dataobj = CIFAR10(self.root, self.train, self.transform, self.target_transform, self.download)
+        # 如果不存在就会下载数据集
+        cifar_dataobj = CIFAR10(self.root, self.train, self.transform, self.target_transform, True)
 
         data = cifar_dataobj.data
         target = np.array(cifar_dataobj.targets)
@@ -55,6 +65,35 @@ class Cifar10Truncated(data.Dataset):
             target = self.target_transform(target)
 
         return img, target
+
+    def __len__(self):
+        return len(self.data)
+
+
+class NodeDataset(data.Dataset):
+
+    def __init__(self, dataset_dir, name, dataset=None):
+        self.name = name
+        self.dataset_dir = dataset_dir
+        self.data = self.load_dataset(dataset)
+
+    def load_dataset(self, dataset):
+        if dataset is None:
+            return self.load_dataset_from_local()
+        else:
+            return dataset
+
+    def load_dataset_from_local(self):
+        try:
+            dataset = torch.load(self.dataset_dir + self.name + dataset_suffix)
+        except Exception:
+            logger.error("节点:{} 无法从本地读取节点数据集", self.name)
+            raise FileNotFoundError
+        logger.info("节点:{} 数据集已加载成功", self.name)
+        return dataset
+
+    def __getitem__(self, index) -> T_co:
+        return self.data.__getitem__(index)
 
     def __len__(self):
         return len(self.data)
