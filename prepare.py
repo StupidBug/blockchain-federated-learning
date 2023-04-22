235d21979e11c46ba444c307087dbb41f1248b5f
leiming <leiming_student@163.com>
2023-04-22 15:57:02 +0800

    更新数据准备工作

--- prepare.py
+++ prepare.py
@@ -1,58 +1,47 @@
-import pickle
-import torchvision.transforms as transforms
 import log
-from datasets import Cifar10Truncated
 
+from utils import *
+from torch.utils.data import Dataset, random_split
+from datasets import GlobalDataset, NodeDataset
 
 logger = log.setup_custom_logger("data")
 
 
-def save_data(dataset, dataset_path):
+def save_data(dataset, filename):
     """
-    将数据集以二进制形式存储起来
-    :param dataset_path: 数据集路径
+    将数据集存储
+    :param filename: 数据集路径
     :param dataset: 数据集
     """
-    with open(dataset_path, "wb") as f:
-        pickle.dump(dataset, f)
+    torch.save(dataset, dataset_dir_path + filename)
 
 
-def load_data(dataset_path):
+def load_data(filename):
     """
     从二进制文件中读取出数据集
-    :param dataset_path: 文件名
+    :param filename: 文件名
     :return: 数据集
     """
-    with open(dataset_path, "rb") as f:
-        return pickle.load(f)
+    return torch.load(dataset_dir_path + filename)
 
 
-def show_dataset_details(dataset, dataset_name):
+def show_dataset_details(dataset):
     """
     展示数据集的基本信息
-    :param dataset_name: 文件
-    :param dataset: 数据集
     """
-    for k in dataset.keys():
-        logger.info("%s: %s: %s", dataset_name, k, dataset[k].shape)
+    node_name = dataset.name
+    length = len(dataset)
+    logger.info("节点:{} 数据集长度:{}".format(node_name, length))
 
 
 def split_dataset(dataset):
     """
     将数据集分割为 n 份
     :param dataset: 数据集
-    :param split_count: 分割份数
     :return: 分割后的数据集
     """
-    datasets = []
-    split_data_length = len(dataset["train_images"]) // split_count
-    for i in range(split_count):
-        datasets.append({
-            "train_images": dataset["train_images"][i * split_data_length:(i + 1) * split_data_length],
-            "train_labels": dataset["train_labels"][i * split_data_length:(i + 1) * split_data_length],
-            "test_images": dataset["test_images"][:],
-            "test_labels": dataset["test_labels"][:],
-        })
+    subset_size = [len(dataset) // split_count] * split_count
+    datasets = random_split(dataset.data, subset_size)
     return datasets
 
 
@@ -63,39 +52,27 @@ def get_cifar10_dataset():
 
     transform = transforms.Compose([transforms.ToTensor()])
 
-    cifar10_train_ds = Cifar10Truncated(dataset_dir_path, train=True, download=True, transform=transform)
-    cifar10_test_ds = Cifar10Truncated(dataset_dir_path, train=False, download=True, transform=transform)
+    train_ds = GlobalDataset(dataset_dir_path, train=True, transform=transform, name="CIFAR10_TRAIN")
+    test_ds = GlobalDataset(dataset_dir_path, train=False, transform=transform, name="CIFAR10_TEST")
 
-    x_train, y_train = cifar10_train_ds.data, cifar10_train_ds.target
-    x_test, y_test = cifar10_test_ds.data, cifar10_test_ds.target
-
-    dataset = {
-        "train_images": x_train,
-        "train_labels": y_train,
-        "test_images": x_test,
-        "test_labels": y_test,
-    }
-
-    return dataset
+    return train_ds, test_ds
 
 
 def prepare_data():
-    dataset_path = dataset_dir_path + "cifar10.d"
-    save_data(get_cifar10_dataset(), dataset_path)
-    cifar10_dataset = load_data(dataset_path)
-    show_dataset_details(cifar10_dataset, "cifar10")
-    for n, d in enumerate(split_dataset(cifar10_dataset, 2)):
-        node_data_name = "federated_data_" + str(n) + ".d"
-        node_data_path = dataset_dir_path + node_data_name
-        save_data(d, node_data_path)
-        dataset = load_data(node_data_path)
-        show_dataset_details(dataset, node_data_name)
+    logger.info("开始数据准备工作————数据本地存放路径:{}".format(dataset_dir_path))
+    train_ds, test_ds = get_cifar10_dataset()
+    show_dataset_details(train_ds)
+    show_dataset_details(test_ds)
+    for n, d in enumerate(split_dataset(train_ds)):
+        node_name = "node_" + str(n)
+        node_dataset = NodeDataset(dataset_dir=dataset_dir_path, name=node_name, dataset=d)
+        show_dataset_details(node_dataset)
 
 
 if __name__ == '__main__':
     # 数据本地存放路径
     dataset_dir_path = "/tmp/dataset/"
     # 数据分割数量
-    split_count = 2
+    split_count = 5
     # 开始准备数据
     prepare_data()
