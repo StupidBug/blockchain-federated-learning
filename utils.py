6cf94f82916fc61a063172b7074dc89704d369ab
leiming <leiming_student@163.com>
2023-05-01 10:13:22 +0800

    增加transform删除高斯噪声

--- utils.py
+++ utils.py
@@ -3,6 +3,9 @@ import pickle
 import torch
 import numpy as np
 from sklearn.metrics import confusion_matrix
+from torchvision.transforms import transforms
+import torch.nn.functional as F
+from torch.autograd import Variable
 import hashlib
 import math
 
@@ -70,29 +73,18 @@ def hash_sha256(text: object):
     return hashlib.sha256(str(pickle.dumps(text)).encode()).hexdigest()
 
 
-class AddGaussianNoise(object):
-    def __init__(self, mean=0., std=1., net_id=None, total=0):
-        self.std = std
-        self.mean = mean
-        self.net_id = net_id
-        self.num = int(math.sqrt(total))
-        if self.num * self.num < total:
-            self.num = self.num + 1
-
-    def __call__(self, tensor):
-        if self.net_id is None:
-            return tensor + torch.randn(tensor.size()) * self.std + self.mean
-        else:
-            tmp = torch.randn(tensor.size())
-            filt = torch.zeros(tensor.size())
-            size = int(28 / self.num)
-            row = int(self.net_id / size)
-            col = self.net_id % size
-            for i in range(size):
-                for j in range(size):
-                    filt[:, row*size+i, col*size+j] = 1
-            tmp = tmp * filt
-            return tensor + tmp * self.std + self.mean
-
-    def __repr__(self):
-        return self.__class__.__name__ + '(mean={0}, std={1})'.format(self.mean, self.std)
+def get_transform():
+    transform_train = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Lambda(lambda x: F.pad(
+            Variable(x.unsqueeze(0), requires_grad=False),
+            (4, 4, 4, 4), mode='reflect').data.squeeze()),
+        transforms.ToPILImage(),
+        transforms.RandomCrop(32),
+        transforms.RandomHorizontalFlip(),
+        transforms.ToTensor(),
+    ])
+    # data prep for test set
+    transform_test = transforms.Compose([
+        transforms.ToTensor()])
+    return transform_train, transform_test
