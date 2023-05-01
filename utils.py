7b65b278a114dc6d8a3ce5c2e9aca2e66b80e82b
leiming <leiming_student@163.com>
2023-05-01 10:05:35 +0800

    增加高斯噪声

--- utils.py
+++ utils.py
@@ -4,6 +4,7 @@ import torch
 import numpy as np
 from sklearn.metrics import confusion_matrix
 import hashlib
+import math
 
 
 def compute_accuracy(model, dataloader, get_confusion_matrix=False, device="cuda"):
@@ -67,3 +68,31 @@ def hash_sha256(text: object):
     :return: 哈希后的结果
     """
     return hashlib.sha256(str(pickle.dumps(text)).encode()).hexdigest()
+
+
+class AddGaussianNoise(object):
+    def __init__(self, mean=0., std=1., net_id=None, total=0):
+        self.std = std
+        self.mean = mean
+        self.net_id = net_id
+        self.num = int(math.sqrt(total))
+        if self.num * self.num < total:
+            self.num = self.num + 1
+
+    def __call__(self, tensor):
+        if self.net_id is None:
+            return tensor + torch.randn(tensor.size()) * self.std + self.mean
+        else:
+            tmp = torch.randn(tensor.size())
+            filt = torch.zeros(tensor.size())
+            size = int(28 / self.num)
+            row = int(self.net_id / size)
+            col = self.net_id % size
+            for i in range(size):
+                for j in range(size):
+                    filt[:, row*size+i, col*size+j] = 1
+            tmp = tmp * filt
+            return tensor + tmp * self.std + self.mean
+
+    def __repr__(self):
+        return self.__class__.__name__ + '(mean={0}, std={1})'.format(self.mean, self.std)
