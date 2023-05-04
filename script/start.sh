007d1ac4299176ea9a6885de134ab71b701e7a0f
leiming <2228561590@qq.com>
2023-05-04 17:51:31 +0800

    增加脚本中dataset选择

--- script/start.sh
+++ script/start.sh
@@ -2,9 +2,10 @@ client_num=2
 dataset_dir="..\\dataset"
 block_dir="..\\block"
 updates_dir="..\\updates"
+dataset_type="medmnist_pathmnist"
 learning_rate=0.01
 
-python ../prepare.py -n=${client_num} -d=${dataset_dir}
+python ../prepare.py -n=${client_num} -d=${dataset_dir} -t=${dataset_type}
 
 # 启动 miner 节点
 python ../miner.py --port=5000 --host="127.0.0.1" --genesis=1 --dataset_dir=${dataset_dir} --block_dir=${block_dir} --update_limit=${client_num} &
